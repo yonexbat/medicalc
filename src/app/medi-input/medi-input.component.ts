ddbb7da56a73f6e5599ee83bf841ef4deb1c14b2
yonexbat@gmail.com <yonexbat@gmail.com>
2017-02-19 18:56:54 +0100

    zwischenstand

--- src/app/medi-input/medi-input.component.ts
+++ src/app/medi-input/medi-input.component.ts
@@ -1,5 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import {MediserviceService} from './../mediservice.service';
+import {Observable} from 'rxjs/Observable';
+import {Subject } from 'rxjs/Subject';
+import {MediData} from '../medi-data';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+
+// Observable operators
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
 
 @Component({
   selector: 'app-medi-input',
@@ -9,13 +22,40 @@ import {MediserviceService} from './../mediservice.service';
 })
 export class MediInputComponent implements OnInit {
 
+  weight: number;
+  dose: number;
+  medisFound: Observable<MediData[]>;
+  private mediSearchTearm = new Subject<string>();
+  medi: MediData;
 
 
   constructor(private mediInputService: MediserviceService) { }
 
   ngOnInit() {
+
+      this.medisFound = this.mediSearchTearm
+      .debounceTime(300)        // wait 300ms after each keystroke before considering the term
+      .distinctUntilChanged()   // ignore if next search term is same as previous
+      .switchMap(term => term   // switch to new observable each time the term changes
+        // return the http search observable
+        ? this.mediInputService.searchMediData(term)
+        // or the observable of empty heroes if there was no search term
+        : Observable.of<MediData[]>([]))
+      .catch(error => {
+        // TODO: add real error handling
+        console.log(error);
+        return Observable.of<MediData[]>([]);
+      });
   }
 
+  // Push a search term into the observable stream.
+  searchMedi(mediTerm: string): void {
+    this.mediSearchTearm.next(mediTerm);
+  }  
 
-
+  selectMedi(medi: MediData)
+  {
+    this.medi = medi;
+    alert("Medi selected");
+  }
 }
--- src/app/mediservice.service.ts
+++ src/app/mediservice.service.ts
@@ -1,18 +1,43 @@
 import { Injectable } from '@angular/core';
-import {MediData} from './medi-data'
+import {MediData} from './medi-data';
+import { Http }       from '@angular/http';
+
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MediserviceService {
 
-  constructor() { }
+  constructor() { } 
 
-  getMediData(): Promise<MediData[]> {
+  public getMediData(): Observable<MediData[]> {
+    let result = this.getMediDataInternal();
+    return Observable.of(result);
+  }
+
+  public searchMediData(searchtearm: string) : Observable<MediData[]> 
+  {
+    let allData = this.getMediDataInternal();
+    let result = [];   
+    allData.forEach(element => {
+      let searchTearmUpperCase = searchtearm.toUpperCase();
+      let mediNameUpperCase = element.Name.toUpperCase();
+
+      if(mediNameUpperCase.includes(searchTearmUpperCase))
+      {
+        result.push(element);
+      }
+    });
+    return Observable.of(result);
+  }
+
+  private getMediDataInternal(): MediData[] {
     let result = [
       new MediData(3, 'Fenta'),
       new MediData(4, 'Cocaine'),
     ];
 
-    return Promise.resolve(result);
+    return result;
   }
 
 }
