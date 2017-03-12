36f89f7c921697e1d46d563697c8d02bc7cb9ab4
yonexbat@gmail.com <yonexbat@gmail.com>
2017-03-12 10:26:55 +0100

    MEDIS als JSON

--- src/app/medi-data.ts
+++ src/app/medi-data.ts
@@ -1,10 +1,23 @@
 export class MediData {
+
     Id: number;
     Name: string; 
+    ActiveSubstance: string;
+    Concentration: number;
+    MinDose: number;
+    MaxDose: number;
 
-    constructor(Id: number, Name: string)
+    constructor(Id: number, Name: string, 
+        ActiveSubstance: string, Concentration: number, 
+        MinDose: number, MaxDose: number)
     {
         this.Id = Id;
         this.Name = Name;
+        this.ActiveSubstance = ActiveSubstance;
+        this.Concentration = Concentration;
+        this.MinDose = MinDose;
+        this.MaxDose = MaxDose;
+
     }
+
 }
--- src/app/mediservice.service.ts
+++ src/app/mediservice.service.ts
@@ -2,50 +2,66 @@ import { Injectable } from '@angular/core';
 import {MediData} from './medi-data';
 import { Http }       from '@angular/http';
 
-import { Observable }     from 'rxjs/Observable';
+//import { Observable }     from 'rxjs/Observable';
+import {Observable} from 'rxjs';
+
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/concatMap';
+
 
 @Injectable()
 export class MediserviceService {
 
-  constructor() { } 
+  private http: Http;
+
+  private url: string;
+
+
+  constructor(http: Http) { 
+    this.http = http;
+    this.url = "./app/medi-data-list.json";
+  } 
 
   public getMediData(): Observable<MediData[]> {
     let result = this.getMediDataInternal();
-    return Observable.of(result);
+    return result;
   }
 
   public searchMediData(searchtearm: string) : Observable<MediData[]> 
-  {
-    let allData = this.getMediDataInternal();
-    let result = [];   
-    allData.forEach(element => {
-      let searchTearmUpperCase = searchtearm.toUpperCase();
-      let mediNameUpperCase = element.Name.toUpperCase();
+  {  
 
-      if(mediNameUpperCase.includes(searchTearmUpperCase))
-      {
-        result.push(element);
-      }
-    });
-    return Observable.of(result);
+    let arrayObservable = this.getMediDataInternal()
+      .map( array => 
+          {
+                let result = [];
+                array.forEach(element => 
+                  {
+
+                    let searchTearmUpperCase = searchtearm.toUpperCase();
+                    let mediNameUpperCase = element.Name.toUpperCase();
+
+                    if(mediNameUpperCase.includes(searchTearmUpperCase))
+                    {
+                      result.push(element);
+                    }
+                  });
+                return result;                 
+         })
+
+    return arrayObservable;   
   }
 
-  private getMediDataInternal(): MediData[] {
-    let result = [
-      new MediData(1, 'Afoxolaner'),
-      new MediData(2, 'Aminoacridin'),
-      new MediData(3, 'Magnesiumsulfat'),
-      new MediData(4, 'Maropitant'),
-      new MediData(5, 'Fenta'),
-      new MediData(6, 'Wismutaluminat'),
-      new MediData(7, 'Meloxicam'),
-      new MediData(8, 'Glucose'),
-      new MediData(9, 'Guajacol'),
-      new MediData(10, 'Cocaine'),
-    ];
+  private getMediDataInternal(): Observable<MediData[]> {
 
-    return result;
+      return this.http
+               .get(this.url)
+               .map(response => { 
+                  let data = response.json();
+                  let medidata = data as MediData[];
+                  return medidata;
+                });      
   }
 
 }
